7d633a8ef234ca87a85c990e71852ea75908f106
maximoortelli <maximoortelli@hotmail.com>
2023-12-07 14:41:29 -0300

    Update posts_controller.rb
    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,26 @@
 class PostsController < ApplicationController
   def index
-    # Catch the user which 'author_id' is the same as the parameter 'user_id' on the URL
     @user = User.find(params[:user_id])
 
-    # Catch all the posts associates to this user and paginate them
     @posts = @user.posts.page(params[:page]).per(3)
+  end
 
-    # @posts = @user.posts.paginate(page: params[:page], per_page: 4)
+  class PostsController < ApplicationController
+    def index
+      user = User.find(params[:user_id])
 
-    # Rest of the logic to show the posts on the view
+      @posts = user.posts
+    end
+
+    def show
+      @post = Post.find(params[:id])
+    end
   end
 
   def show
-    # Found the post which 'id' is the same as the parameter 'id' on the URL
     @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
     @next_post = @user.posts.where('id > ?', @post.id).first
     @prev_post = @user.posts.where('id < ?', @post.id).last
-    # Rest of the logic to show the information of the post on the view
   end
 end
