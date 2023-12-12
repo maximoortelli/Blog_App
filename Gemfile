e2c590094a12e6b91b166ba782b38b57d93581c1
maximoortelli <maximoortelli@hotmail.com>
2023-12-12 18:02:11 -0300

    Add all files
    Add all files

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'rubocop', '>= 1.0', '< 2.0'
 gem 'kaminari'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem 'rails', '~> 7.0.8'
+gem 'rails', '~> 7.1'
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem 'sprockets-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_27_153419) do
+ActiveRecord::Schema[7.1].define(version: 2023_09_27_153419) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "adminpack"
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
@@ -36,8 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_153419) do
     t.bigint "author_id"
     t.string "title"
     t.text "text"
-    t.integer "comments_counter", default: 0
-    t.integer "likes_counter", default: 0
+    t.integer "comments_counter"
+    t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "content"
@@ -48,7 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_153419) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "post_counter", default: 0
+    t.integer "post_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
